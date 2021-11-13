4fd3cc39beebe6470e1f9b314fe686f286aeb4e2
HenriqueJRodrigues <90482784+HenriqueJRodrigues@users.noreply.github.com>
2021-11-12 23:57:19 -0300

    Update paleta_images.dart

--- lib/paleta_images.dart
+++ lib/paleta_images.dart
@@ -1,3 +1,7 @@
 class AppImages {
   static const perfil = "images/henrique.jpg";
+  static const fundo = "images/fundo3.jpg";
+  static const linkd = "images/LinkedIn.png";
+  static const github = "images/githubt.png";
+  static const gmail = "images/gmail.png.jpg";
 }
